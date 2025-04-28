cef65337493354f102d63ff6c62aa2b447b91936
Ben Peake <benpeake@Bens-MacBook-Pro.local>
2025-04-28 13:13:35 +0100

    service container

---
+++ web/app/themes/sleep/views/blocks/service-hero/fields.php
@@ -0,0 +1,16 @@
+<?php
+
+use YPTheme\AcfBuilder\ThemeFieldBuilder;
+
+$block = new ThemeFieldBuilder('service-hero');
+$block
+    ->getClone('heading')
+    ->addWysiwyg('content')
+    ->addImage('hero_image_primary', ['label' => 'Primary screen visual'])
+    ->addImage('hero_image_secondary', ['label' => 'Secondary screen visual'])
+    ->addRepeater('points', ['max' => 10])
+    ->addImage('icon')
+    ->addText('point')
+    ->endRepeater()
+    ->setLocation('block', '==', 'acf/service-hero')
+    ->setFields();
