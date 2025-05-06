c52c9b072fe9f7df1b8bd3b31ec9e103e5bd352b
Ben Peake <benpeake@Bens-MacBook-Pro.local>
2025-05-06 12:09:49 +0100

    adding germany to map

--- web/app/themes/sleep/views/blocks/map/fields.php
+++ web/app/themes/sleep/views/blocks/map/fields.php
@@ -12,5 +12,9 @@ $block
         'label' => 'Display US office?',
         'ui' => 1,
     ])
+    ->addTrueFalse('show_berlin', [
+        'label' => 'Display Berlin office?',
+        'ui' => 1,
+    ])
     ->setLocation('block', '==', 'acf/map')
     ->setFields();
