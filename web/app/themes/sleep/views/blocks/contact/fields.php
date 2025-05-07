8e612756171c3b5a7f5dda71649f5be870c744c6
Ben Peake <benpeake@Bens-MBP.fritz.box>
2025-05-07 10:20:39 +0100

    adding hubspot embed option on contact form

--- web/app/themes/sleep/views/blocks/contact/fields.php
+++ web/app/themes/sleep/views/blocks/contact/fields.php
@@ -7,6 +7,12 @@ $block
     ->addText('subheading')
     ->getClone('heading')
     ->addWysiwyg('content')
+    ->addTrueFalse('gravity_form', [
+        'label' => 'Use Gravity form?',
+        'instructions' => 'if not hubspot embed will be used',
+        'ui' => 1,
+    ])
     ->addNumber('form_id')
+    ->conditional('gravity_form', '==', '1')
     ->setLocation('block', '==', 'acf/contact')
     ->setFields();
